a25bda2c6f9f924e7f01071e64caa421f600a1d8
Matthew Francis <mattfrancis888@gmail.com>
2022-01-07 01:18:11 -0500

    Switched to webpack sass laoder.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import "./css/main.css";
+// import "./css/main.css";
+
+import "./scss/main.scss";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
