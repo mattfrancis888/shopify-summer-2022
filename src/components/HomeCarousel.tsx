32a15b9e12e7b79fada81eb8cd1ecb4ea0118cf0
Matthew Francis <mattfrancis888@gmail.com>
2022-01-07 15:35:37 -0500

    Made progress in bem refactor.

--- src/components/HomeCarousel.tsx
+++ src/components/HomeCarousel.tsx
@@ -19,44 +19,44 @@ const slides = [
     {
         title: "Pic1",
         mobileImage:
-            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1631384571/shopify2/wise_nebula.jpg",
-        image: "https://images.firstpost.com/fpimages/1200x800/fixed/jpg/2020/05/helicopter_landing_composite_crop-web-1.jpg",
+            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1641573668/shopify-summer-2022/helicopter_landing_composite_crop-web-1.jpg",
+        image: "https://res.cloudinary.com/du8n2aa4p/image/upload/v1641573668/shopify-summer-2022/helicopter_landing_composite_crop-web-1.jpg",
         logo: "https://res.cloudinary.com/du8n2aa4p/image/upload/v1631384602/shopify2/5ecec99f73e4440004f09e7c.png",
         description: "Meet Ingenunity - Mars' Helicopter ",
         fallbackMobileImage:
-            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1620418766/shopify1/Avengers-Infinity-War-Poster.jpg",
+            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1641573668/shopify-summer-2022/helicopter_landing_composite_crop-web-1.jpg",
         fallbackImage:
-            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1620418766/shopify1/Avengers-Infinity-War-Poster.jpg",
+            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1641573668/shopify-summer-2022/helicopter_landing_composite_crop-web-1.jpg",
         fallbackLogo:
-            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1631384602/shopify2/5ecec99f73e4440004f09e7c.png",
+            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1641573668/shopify-summer-2022/helicopter_landing_composite_crop-web-1.jpg",
     },
     {
         title: "Pic2",
         mobileImage:
-            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1631384716/shopify2/106194367-1571709112694gettyimages-103017185.jpg",
-        image: "https://wallpaperaccess.com/full/1747542.jpg",
+            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1641573688/shopify-summer-2022/1747542.jpg",
+        image: "https://res.cloudinary.com/du8n2aa4p/image/upload/v1641573688/shopify-summer-2022/1747542.jpg",
         logo: "https://res.cloudinary.com/du8n2aa4p/image/upload/v1631384602/shopify2/5ecec99f73e4440004f09e7c.png",
         description: "Meet Perseverance - Mars' Rover",
         fallbackMobileImage:
-            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1631384716/shopify2/106194367-1571709112694gettyimages-103017185.jpg",
+            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1641573688/shopify-summer-2022/1747542.jpg",
         fallbackImage:
-            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1631384716/shopify2/106194367-1571709112694gettyimages-103017185.jpg",
+            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1641573688/shopify-summer-2022/1747542.jpg",
         fallbackLogo:
             "https://res.cloudinary.com/du8n2aa4p/image/upload/v1631384602/shopify2/5ecec99f73e4440004f09e7c.png",
     },
     {
         title: "Pic3",
         mobileImage:
-            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1631384658/shopify2/helixnebula-169.jpg",
+            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1641573704/shopify-summer-2022/nasa_name_main-1280x720.jpg",
         // image:
         //   "https://res.cloudinary.com/du8n2aa4p/image/upload/v1631384658/shopify2/helixnebula-169.jpg",
-        image: "https://www.slashgear.com/wp-content/uploads/2019/08/nasa_name_main-1280x720.jpg",
+        image: "https://res.cloudinary.com/du8n2aa4p/image/upload/v1641573704/shopify-summer-2022/nasa_name_main-1280x720.jpg",
         logo: "https://res.cloudinary.com/du8n2aa4p/image/upload/v1631384602/shopify2/5ecec99f73e4440004f09e7c.png",
         description: "Name our next rover",
         fallbackMobileImage:
-            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1631384658/shopify2/helixnebula-169.jpg",
+            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1641573704/shopify-summer-2022/nasa_name_main-1280x720.jpg",
         fallbackImage:
-            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1631384658/shopify2/helixnebula-169.jpg",
+            "https://res.cloudinary.com/du8n2aa4p/image/upload/v1641573704/shopify-summer-2022/nasa_name_main-1280x720.jpg",
         fallbackLogo:
             "https://res.cloudinary.com/du8n2aa4p/image/upload/v1631384602/shopify2/5ecec99f73e4440004f09e7c.png",
     },
@@ -134,7 +134,7 @@ const HomeCarousel: React.FC<{}> = () => {
         });
 
         anime({
-            targets: ".slideImageLogoAndDescWrap",
+            targets: ".slide__image-logo-and-desc",
             opacity: [
                 {
                     value: [0, 1],
@@ -257,12 +257,12 @@ const HomeCarousel: React.FC<{}> = () => {
                          {transition((style, item) => {
                             return (
                                 <div
-                                    className={`slideImageLogoAndDescWrap
+                                    className={`slide__image-logo-and-desc
                                     `}
                                 >
                                     <animated.div style={style}>
                                         <img
-                                            className="slideImageLogo"
+                                            className="slide__image-logo"
                                             src={slide.logo}
                                             alt="game slide"
                                             onError={(e: any) => {
@@ -270,7 +270,7 @@ const HomeCarousel: React.FC<{}> = () => {
                                                 e.target.src = `${slide.fallbackLogo}`;
                                             }}
                                         ></img>
-                                        <h1 className="slideDesc">
+                                        <h1 className="slide__desc">
                                             {slide.description}
                                         </h1>
                                     </animated.div>
@@ -278,12 +278,12 @@ const HomeCarousel: React.FC<{}> = () => {
                             );
                         })} */}
                         <div
-                            className={`slideImageLogoAndDescWrap
+                            className={`slide__image-logo-and-desc
                                     `}
                         >
                             <div>
                                 <img
-                                    className="slideImageLogo"
+                                    className="slide__image-logo"
                                     src={slide.logo}
                                     alt="game slide"
                                     onError={(e: any) => {
@@ -291,7 +291,7 @@ const HomeCarousel: React.FC<{}> = () => {
                                         e.target.src = `${slide.fallbackLogo}`;
                                     }}
                                 ></img>
-                                <h1 className="slideDesc">
+                                <h1 className="slide__desc">
                                     {slide.description}
                                 </h1>
                             </div>
